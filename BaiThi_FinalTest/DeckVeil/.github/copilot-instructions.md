1b18c02cd27bde7b1dcf83f728fa4aaaf79489ff
DatZ34 <vothanhdat4265@gmail.com>
2025-09-04 23:06:35 +0700

    Bắt đầu ngầy 9/4/2025,

---
+++ BaiThi_FinalTest/DeckVeil/.github/copilot-instructions.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: DeckVeil
+- Unity version: Unity 6000.0.46f1
+- Active game object:
+  - Name: Player
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ BaiThi_FinalTest/DeckVeil/Assets/Scripts/Player/CharacterBase.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CharacterBase : MonoBehaviour
+{
+    [Header("Status")]
+    [SerializeField] protected float speed = 5;
+
+    [Header("Component Add")]
+    [SerializeField] protected Rigidbody2D rb;
+
+    [Header("Dynamic Information")]
+    [SerializeField] protected float horizontal;
+    [SerializeField] protected float vertical;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected virtual void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        Move();
+    }
+    protected virtual void Move()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        // Chuyển input sang hướng isometric
+        Vector2 isoMovement = new Vector2(
+            input.x - input.y,       // X hướng isometric
+            (input.x + input.y) / 2  // Y hướng isometric
+        );
+        rb.MovePosition(rb.position + isoMovement * speed * Time.fixedDeltaTime);
+
+    }
+}
